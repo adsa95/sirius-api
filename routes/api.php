6dcb8b96e5d479c0ab38ed515f293f45e3ba9d16
Adam Sandström <adsa95@gmail.com>
2017-02-01 16:16:02 +0100

    Add routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Plugin;
+use App\Config;
+use App\Exceptions\SlackException;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,49 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/up', function (Request $request) {
+    return '{"ok": true}';
+});
+
+Route::get('/plugins', function(Request $request){
+	return Plugin::all();
+});
+
+Route::get('/configs', function(Request $request){
+	//TODO: Only return users if correct sirius-token is provided!
+	return Config::all();
+});
+
+Route::get('/configs/{token}', function(Request $request, $token){
+	return Config::where('slack_token', '=', $token)->firstOrFail();
+});
+
+Route::post('/configs', function(Request $request){
+	$data = $request->all();
+
+	$model = Config::where('slack_token', '=' , $data['slack_token'])->first();
+
+	if($model == null){
+		$slackData = json_decode(file_get_contents('https://slack.com/api/auth.test?token='.$data['slack_token']));
+		if($slackData->ok == false){
+			throw new SlackException;
+		}
+
+		$slack_ids = $slackData->team_id.'.'.$slackData->user_id;
+		$model = Config::where('slack_ids', '=', $slack_ids)->first();
+	}
+
+	if($model == null){
+		$model = new Config;
+		if(isset($slack_ids)){
+			$model->slack_ids = $slack_ids;
+		}else{
+			throw new SlackException;
+		}
+	}
+
+	$model->fill($data);
+	$model->save();
+
+	return $model->fresh();
 });
