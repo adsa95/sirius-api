45ed01a64ab6f2b409ac4b5225885367c96a8127
Adam Sandström <adsa95@gmail.com>
2017-02-07 13:11:51 +0100

    Add id_hash_sha256 property to config JSON model

--- app/Config.php
+++ app/Config.php
@@ -3,11 +3,17 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Hash;
 
 class Config extends Model
-{	
+{
 	protected $fillable = ['config', 'slack_token'];
 	protected $hidden = ['slack_ids'];
+	protected $appends = ['id_hash_sha256'];
+
+	public function getIdHashSha256Attribute(){
+		return hash('sha256', $this->attributes['slack_ids']);
+	}
 
     public function getConfigAttribute($value){
     	if(is_null($value)) return $value;
