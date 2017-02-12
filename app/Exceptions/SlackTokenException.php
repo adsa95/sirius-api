fd83d1b4bd841ed5b107838bc765f2d2a4479bd8
Johan Vester <jv@jv-macbook.local>
2017-02-12 15:18:34 +0100

    Remove wrong header (oops)

--- app/Exceptions/SlackTokenException.php
+++ app/Exceptions/SlackTokenException.php
@@ -2,15 +2,6 @@
 
 namespace App\Exceptions;
 
-/**
- * Created by Johan Vester
- * johan@nicknamed.se
- *
- * Date: 2017-02-12
- *
- * (c) 2016 Nicknamed
- */
-
 class SlackTokenException extends SlackException
 {
     public static function invalidToken(): SlackTokenException
